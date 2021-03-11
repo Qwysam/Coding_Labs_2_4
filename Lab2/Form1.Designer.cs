a4ea083f305743bc22378b55c21c4d38a9bced99
Vsevolod Zhdanov <61472372+Qwysam@users.noreply.github.com>
2021-03-11 13:55:50 +0300

    Lab2

---
+++ Lab2/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿using System.Windows.Forms;
+namespace Lab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.variable_a = new System.Windows.Forms.TextBox();
+            this.variable_b = new System.Windows.Forms.TextBox();
+            this.variable_c = new System.Windows.Forms.TextBox();
+            this.First_Method = new System.Windows.Forms.RadioButton();
+            this.Second_Method = new System.Windows.Forms.RadioButton();
+            this.Third_Method = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Run = new System.Windows.Forms.Button();
+            this.Clear = new System.Windows.Forms.Button();
+            this.x1 = new System.Windows.Forms.TextBox();
+            this.x2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // variable_a
+            // 
+            this.variable_a.Location = new System.Drawing.Point(62, 115);
+            this.variable_a.Margin = new System.Windows.Forms.Padding(6);
+            this.variable_a.Name = "variable_a";
+            this.variable_a.Size = new System.Drawing.Size(196, 31);
+            this.variable_a.TabIndex = 0;
+            this.variable_a.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.variable_a_KeyPress);
+            // 
+            // variable_b
+            // 
+            this.variable_b.Location = new System.Drawing.Point(62, 202);
+            this.variable_b.Margin = new System.Windows.Forms.Padding(6);
+            this.variable_b.Name = "variable_b";
+            this.variable_b.Size = new System.Drawing.Size(196, 31);
+            this.variable_b.TabIndex = 1;
+            this.variable_b.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.variable_b_KeyPress);
+            // 
+            // variable_c
+            // 
+            this.variable_c.Location = new System.Drawing.Point(62, 288);
+            this.variable_c.Margin = new System.Windows.Forms.Padding(6);
+            this.variable_c.Name = "variable_c";
+            this.variable_c.Size = new System.Drawing.Size(196, 31);
+            this.variable_c.TabIndex = 2;
+            this.variable_c.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.variable_c_KeyPress);
+            // 
+            // First_Method
+            // 
+            this.First_Method.AutoSize = true;
+            this.First_Method.Checked = true;
+            this.First_Method.Location = new System.Drawing.Point(86, 413);
+            this.First_Method.Name = "First_Method";
+            this.First_Method.Size = new System.Drawing.Size(125, 29);
+            this.First_Method.TabIndex = 3;
+            this.First_Method.TabStop = true;
+            this.First_Method.Text = "Метод 1";
+            this.First_Method.UseVisualStyleBackColor = true;
+            // 
+            // Second_Method
+            // 
+            this.Second_Method.AutoSize = true;
+            this.Second_Method.Location = new System.Drawing.Point(86, 471);
+            this.Second_Method.Name = "Second_Method";
+            this.Second_Method.Size = new System.Drawing.Size(125, 29);
+            this.Second_Method.TabIndex = 4;
+            this.Second_Method.Text = "Метод 2";
+            this.Second_Method.UseVisualStyleBackColor = true;
+            // 
+            // Third_Method
+            // 
+            this.Third_Method.AutoSize = true;
+            this.Third_Method.Location = new System.Drawing.Point(86, 528);
+            this.Third_Method.Name = "Third_Method";
+            this.Third_Method.Size = new System.Drawing.Size(125, 29);
+            this.Third_Method.TabIndex = 5;
+            this.Third_Method.Text = "Метод 3";
+            this.Third_Method.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.label1.Location = new System.Drawing.Point(13, 115);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 25);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "a";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.label2.Location = new System.Drawing.Point(13, 202);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 25);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "b";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.label3.Location = new System.Drawing.Point(13, 288);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 25);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "c";
+            // 
+            // Run
+            // 
+            this.Run.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.Run.Location = new System.Drawing.Point(410, 413);
+            this.Run.Name = "Run";
+            this.Run.Size = new System.Drawing.Size(184, 56);
+            this.Run.TabIndex = 9;
+            this.Run.Text = "Запустить";
+            this.Run.UseVisualStyleBackColor = true;
+            this.Run.Click += new System.EventHandler(this.Run_Click);
+            // 
+            // Clear
+            // 
+            this.Clear.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.Clear.Location = new System.Drawing.Point(410, 501);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(184, 56);
+            this.Clear.TabIndex = 10;
+            this.Clear.Text = "Очистить";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // x1
+            // 
+            this.x1.Location = new System.Drawing.Point(398, 115);
+            this.x1.Margin = new System.Windows.Forms.Padding(6);
+            this.x1.Name = "x1";
+            this.x1.ReadOnly = true;
+            this.x1.Size = new System.Drawing.Size(196, 31);
+            this.x1.TabIndex = 11;
+            // 
+            // x2
+            // 
+            this.x2.Location = new System.Drawing.Point(398, 202);
+            this.x2.Margin = new System.Windows.Forms.Padding(6);
+            this.x2.Name = "x2";
+            this.x2.ReadOnly = true;
+            this.x2.Size = new System.Drawing.Size(196, 31);
+            this.x2.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.label4.Location = new System.Drawing.Point(346, 121);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 25);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "x1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.label5.Location = new System.Drawing.Point(346, 208);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 25);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "x2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Desktop;
+            this.ClientSize = new System.Drawing.Size(659, 608);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.x2);
+            this.Controls.Add(this.x1);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.Run);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Third_Method);
+            this.Controls.Add(this.Second_Method);
+            this.Controls.Add(this.First_Method);
+            this.Controls.Add(this.variable_c);
+            this.Controls.Add(this.variable_b);
+            this.Controls.Add(this.variable_a);
+            this.ForeColor = System.Drawing.Color.White;
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox variable_a;
+        private System.Windows.Forms.TextBox variable_b;
+        private System.Windows.Forms.TextBox variable_c;
+        private System.Windows.Forms.RadioButton First_Method;
+        private System.Windows.Forms.RadioButton Second_Method;
+        private System.Windows.Forms.RadioButton Third_Method;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button Run;
+        private System.Windows.Forms.Button Clear;
+        private TextBox x1;
+        private TextBox x2;
+        private Label label4;
+        private Label label5;
+    }
+}
+
---
+++ Lab2/Form1.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab2
+{
+    
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        void Roots(double a ,double b, double c)
+        {
+            double D = b * b - 4 * a * c;
+            if (D > 0)
+            {
+                try
+                {
+                    double x1_res = Math.Round((-b + Math.Sqrt(D)) / (2 * a), 5);
+                    double x2_res = Math.Round((-b - Math.Sqrt(D)) / (2 * a), 5);
+                    x1.Text = x1_res.ToString();
+                    x2.Text = x2_res.ToString();
+                }
+                catch { }
+            }
+            if (D == 0)
+            {
+                try
+                {
+                    double x1_res = Math.Round(-b / (2 * a), 5);
+                    x1.Text = x1_res.ToString();
+                }
+                catch { }
+                x2.Text = "";
+            }
+            if (D < 0)
+            {
+                x1.Text = "Дискриминант";
+                x2.Text = "равен нулю";
+            }
+
+        }
+
+        private void CheckInput(TextBox textBox, object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && e.KeyChar != (char)Keys.Back)
+            {
+                if (e.KeyChar == '-')
+                {
+                    if (textBox.Text.Length > 0 || textBox.Text.Contains('-'))
+                        e.KeyChar = (char)Keys.None;
+                }
+                if (e.KeyChar == '+')
+                    if (textBox.Text.Length > 0 || textBox.Text.Contains('+'))
+                        e.KeyChar = (char)Keys.None;
+                if (e.KeyChar == ',')
+                {
+                    if (textBox.Text.Contains(',') || textBox.Text.Length == 0)
+                        e.KeyChar = (char)Keys.None;
+                }
+                if (e.KeyChar != '-' && e.KeyChar != '+' && e.KeyChar != ',')
+                    e.KeyChar = (char)Keys.None;
+            }
+        }
+        private void variable_a_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            CheckInput(variable_a, sender, e);
+        }
+
+        private void variable_b_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            CheckInput(variable_b, sender, e);
+        }
+        private void variable_c_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            CheckInput(variable_c, sender, e);
+        }
+
+        private void Run_Click(object sender, EventArgs e)
+        {
+            if (First_Method.Checked)
+            {
+                try
+                {
+                    double a = Convert.ToDouble(variable_a.Text);
+                    double b = Convert.ToDouble(variable_b.Text);
+                    double c = Convert.ToDouble(variable_c.Text);
+                    double D = b * b - 4 * a * c;
+                    if (D > 0)
+                    {
+                        double x1_res = Math.Round((-b + Math.Sqrt(D)) / (2 * a),5);
+                        double x2_res = Math.Round((-b - Math.Sqrt(D)) / (2 * a),5);
+                        x1.Text = x1_res.ToString();
+                        x2.Text = x2_res.ToString();
+                    }
+                    if (D == 0)
+                    {
+                        double x1_res = Math.Round(-b / (2 * a),5);
+                        x1.Text = x1_res.ToString();
+                        x2.Text = "";
+                    }
+                    if (D < 0)
+                    {
+                        x1.Text = "Дискриминант";
+                        x2.Text = "равен нулю";
+                    }
+                }
+                catch (OverflowException of)
+                {
+
+                }
+
+            }
+            if (Second_Method.Checked)
+            {
+                try
+                {
+                    double a = Convert.ToDouble(variable_a.Text);
+                    double b = Convert.ToDouble(variable_b.Text);
+                    double c = Convert.ToDouble(variable_c.Text);
+                    Roots(a, b, c);
+
+                }
+                catch
+                {
+
+                }
+            }
+            if (Third_Method.Checked)
+            {
+                try
+                {
+                    double a = Convert.ToDouble(variable_a.Text);
+                    double b = Convert.ToDouble(variable_b.Text);
+                    double c = Convert.ToDouble(variable_c.Text);
+                    Calculation calc = new Calculation();
+                    double?[]res = calc.ShowRoots(a, b, c);
+                    if (res[0] != null && res[1] != null)
+                    {
+                        x1.Text = res[0].ToString();
+                        x2.Text = res[1].ToString();
+                    }
+                    if(res[0]!= null && res[1] == null)
+                    {
+                        x1.Text = res[0].ToString();
+                        x2.Text = "";
+                    }
+                    if (res[0] == null && res[1] == null)
+                    {
+                        x1.Text = "Дискриминант";
+                        x2.Text = "равен нулю";
+                    }
+                }
+                catch
+                {
+
+                }
+            }
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            variable_a.Text = "";
+            variable_b.Text = "";
+            variable_c.Text = "";
+        }
+    }
+    public class Calculation
+    {
+        public double?[] ShowRoots(double a, double b, double c)
+        {
+            double?[] results = new double?[2];
+            double D = b * b - 4 * a * c;
+            if (D > 0)
+            {
+                try
+                {
+                    double x1_res = Math.Round((-b + Math.Sqrt(D)) / (2 * a), 5);
+                    double x2_res = Math.Round((-b - Math.Sqrt(D)) / (2 * a), 5);
+                    results[0] = x1_res;
+                    results[1] = x2_res;
+                }
+                catch { }
+            }
+            if (D == 0)
+            {
+                try
+                {
+                    double x1_res = Math.Round(-b / (2 * a), 5);
+                    results[0] = x1_res;
+                }
+                catch { }
+                results[1] = null;
+            }
+            if (D < 0)
+            {
+                results[0] = null;
+                results[1] = null;
+            }
+            return results;
+        }
+    }
+}
